651aa8bdcfda183f95c7ccf51ba435e5577f31bf
root <root@DESKTOP-IVHFQKO.localdomain>
2025-10-17 13:04:13 -0600

    Fix s3

--- app/Models/File.php
+++ app/Models/File.php
@@ -22,7 +22,7 @@ class File extends Model
         if(app()->isProduction()){
 
 
-            info($this->url , config('services.ses.ruta_archivos'));
+            info(config('services.ses.ruta_archivos'));
             if(Str::contains($this->url, config('services.ses.ruta_archivos'))){
 
 
